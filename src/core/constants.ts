d41c4cc78c25cd4f6c89dc157800bac59f458f2b
Aleksey <aleksey.o@it-dimension.com>
2022-08-01 11:15:04 +0300

    version change

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '121.0.0.29.119';
+export const APP_VERSION = '230.0.0.16.108';
 export const APP_VERSION_CODE = '185203708';
 export const SIGNATURE_KEY = '9193488027538fd3450b83b7d05286d4ca9599a0f7eeed90d8c85925698a05dc';
 export const BREADCRUMB_KEY = 'iN4$aGr0m';
